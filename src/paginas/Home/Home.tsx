6488df600450db03a71b4c958fcc9bb74fce37af
Jean Franco <jeanoliveirafranco@gmail.com>
2022-08-30 20:17:31 -0300

    [Primeiros passos em react.js]

---
+++ src/paginas/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+  return (
+    <>
+      <h1 className="titulo">Home</h1>
+      <img src="https://cdn.pixabay.com/photo/2022/04/07/12/00/simpsons-7117371_960_720.png" alt="Homer" className="img"/>
+    </>
+  );
+}
+
+export default Home;
