d3f99aeaadc9d5f90d903013613d7555ea9216aa
Jean Franco <jeanoliveirafranco@gmail.com>
2022-09-07 17:04:19 -0300

    Login/Cadastro/Home/CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/estaticos/navbar/Navbar'
+import Footer from './components/estaticos/footer/Footer'
+import Login from './paginas/login/Login'
 import Home from './paginas/Home/Home'
-
-import './App.css';
-
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario'
+import './App.css'
 
 function App() {
   return (
-   <Home />
-  );
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes>
+          {' '}
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- src/paginas/Home/Home.tsx
+++ src/paginas/Home/Home.tsx
@@ -1,13 +1,62 @@
-import React from 'react';
-import './Home.css';
+import React from 'react'
+import { Button, Box,Grid,Typography} from '@material-ui/core'
+import './Home.css'
 
-function Home(){
+function Home() {
   return (
     <>
-      <h1 className="titulo">Home</h1>
-      <img src="https://cdn.pixabay.com/photo/2022/04/07/12/00/simpsons-7117371_960_720.png" alt="Homer" className="img"/>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        className="caixa"
+      >
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h3"
+              align="center"
+              className="titulo"
+            >
+              {' '}
+              Seja bem vindo(a)!
+            </Typography>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h5"
+              align="center"
+              className="titulo"
+            >
+              expresse aqui os seus pensamentos e opiniões!
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Box marginRight={1}>              
+            </Box>
+            <Button
+              variant="outlined" className='botao'          
+            >
+              Ver Postagens
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src="https://files.passeidireto.com/2e0c40eb-394d-4dc0-848e-8d7d7cc0b3a7/2e0c40eb-394d-4dc0-848e-8d7d7cc0b3a7.jpeg"
+            alt=""
+            style={{ width: '100%', height: '100%' }}
+          ></img>
+        </Grid>
+        <Grid xs={12} style={{ backgroundColor: 'white' }}></Grid>
+      </Grid>
     </>
-  );
+  )
 }
 
-export default Home;
+export default Home
